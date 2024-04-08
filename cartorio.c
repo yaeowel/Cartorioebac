941df571421f07dfee859f845c1ecd6b22493729
yaeowel <166451755+yaeowel@users.noreply.github.com>
2024-04-08 19:15:40 -0300

    Opção de saida
    Incluso opção de saida para o usuário.

--- cartorio.c
+++ cartorio.c
@@ -126,6 +126,7 @@ int main()
 	   printf("\t1 - Registrar nomes\n");
 	   printf("\t2 - Consultar nomes \n");
 	   printf("\t3 - Deletar nomes\n\n");
+	   printf("\t4 - Sair do sistema\n\n");
 	   printf("Opção:"); //Fim do menu
 	
 	   scanf("%d", &opcao); //Armagenando a escolha do usuário
@@ -147,7 +148,11 @@ int main()
 		    deletar();
 		    break;
 		    
-		    default:
+		    case 4:
+		    printf("Obrigado por utilizar o sistema!\n");
+		    return 0;
+		    
+			default:
 		    printf("Essa opção não está disponivel!\n");
 		    system("pause");
 	    }
